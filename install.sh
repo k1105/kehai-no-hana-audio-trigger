0c8e494e4a6697345171aac2b597e4107bc3d326
k1105 <kanapon1105@yahoo.co.jp>
2024-10-28 16:51:10 +0900

    audio-triggerサービスファイルの追加

--- install.sh
+++ install.sh
@@ -5,3 +5,5 @@ sudo apt-get update
 source myenv/bin/activate
 sudo apt-get install libportaudio2 libportaudiocpp0 portaudio19-dev python3-dev
 pip install -r requirements.txt
+mv audio-trigger.service /etc/systemd/system/audio-trigger.service
+sudo systemctl daemon-reload
