a9187296979ef073a6224dfbe2c31f0c417348bb
k1105 <kanapon1105@yahoo.co.jp>
2024-11-07 12:21:29 +0900

    必要な設定の追加

--- start.sh
+++ start.sh
@@ -1,5 +1,16 @@
-#!bin/bash
-cd /home/pi/Desktop/kehai-no-hana-audio-trigger
-export GOOGLE_APPLICATION_CREDENTIALS=credentials/kehai-no-hana-test.json
+#!/bin/bash
+cd /home/pi/Desktop/kehai-no-hana-audio-trigger/
+
+# 環境変数の設定
+export PULSE_SERVER=127.0.0.1
+export ALSA_CONFIG_PATH=/etc/asound.conf
+export PATH=$PATH:/usr/bin
+
+# デバッグ情報の表示
+echo "Activating virtual environment..."
 source myenv/bin/activate
+echo "Virtual environment activated."
+
+echo "Starting Python script..."
 python main.py
+echo "Python script finished with exit code $?"
